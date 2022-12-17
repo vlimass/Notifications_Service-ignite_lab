1c0837d5f48c75b9e3dc877319de720f3c45a69f
Vinícius Lima <villylss@gmail.com>
2022-12-17 16:51:29 -0300

    aula 2 finalizada

--- src/application/use-cases/send-notification.ts
+++ src/application/use-cases/send-notification.ts
@@ -1,6 +1,7 @@
+import { Injectable } from "@nestjs/common";
 import { Content } from "../entities/content";
 import { Notification } from "../entities/notification";
-import { NotificationsRepository } from "../repositories/notifications-repositories";
+import { NotificationsRepository } from "../repositories/notifications-repository";
 
 interface SendNotificationRequest {
   recipientId: string;
@@ -12,6 +13,7 @@ interface SendNotificationResponse {
   notification: Notification;
 }
 
+@Injectable()
 export class SendNotification {
   constructor(private notificationsRepository: NotificationsRepository) {}
 
